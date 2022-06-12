25dab291428c378ec9a245ffbefbeee709615d7f
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-06-12 20:07:14 -0300

    esta tudo errado mas vou arrumar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 // NAVEGAÇÃO
-
 const voltarMenu = document.querySelector("#voltarMenu") 
 const menu = document.querySelector("#menuId")
 const adicionarPalavra = document.querySelector("#menuAdicionarPalavra")
+const jogo = document.querySelector("#jogoId")
 
 function iniciaJogo() {
   voltarMenu.classList.remove("escondido")
   menu.classList.add("escondido")
+  jogo.classList.remove("escondido")
 }
 function backToMenu() {
   menu.classList.remove("escondido")
   voltarMenu.classList.add("escondido")
   adicionarPalavra.classList.add("escondido")
-  console.log(adicionarPalavra)
+  jogo.classList.add("escondido")
+
 }
 function adicionar() {
   menu.classList.add("escondido")
@@ -20,4 +22,44 @@ function adicionar() {
   adicionarPalavra.classList.remove("escondido")
 }
 
+// JOGO
+const palavras = ["DESIGN", "HTML", "COISAS", "FELIZ"]
+const sorteiaPalavra = palavras[Math.floor(Math.random()*palavras.length)]
+const letrasErradas = []
+const letrasCertas = []
+
+document.addEventListener("keydown", (evento) => {
+  const code = evento.keyCode;
+  if (isLetra(code)) {
+    const letra = evento.key;
+    if (letrasErradas.includes(letra)) {
+      mostrarAvisoLetraRepetida()
+    } else {
+      if (sorteiaPalavra.includes(letra)){
+        letrasCertas.push(letra)
+      } else {
+        letrasErradas.push(letra)
+      }
+    }
+    atualizarJogo();
+  }
+})
+
+function atualizarJogo(){
+   mostrarLetrasErradas();
+}
+
+function mostrarLetrasErradas() {
+  const div = document.querySelector(".letrasErros")
+  letrasErradas.forEach(letra => {
+    div.innerHTML += `<span>${letra}</span>`;
+  })
+}
+
+function mostrarAvisoLetraRepetida() {
+  
+}
+function isLetra(code) {
+  return code >= 65 && code <= 90
+}
 
